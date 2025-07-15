f9fe100fc5ab2b21e1396b2513aa695e627be2a5
Victor koome <koomev786@gmail.com>
2025-07-15 12:40:18 +0300

    set up daisy ui library

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Hello world</h1>
-      <Link href="/users">Users</Link>
+      <h1 className="p-3 bg-amber-600">Hello world</h1>
+      <Link href="/users">
+        <button className="btn btn-primary">Primary</button>
+      </Link>
+
     </main>
   );
 }
