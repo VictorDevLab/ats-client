5c17ce642525e55a92f57cf582498242840e6ba9
Victor koome <koomev786@gmail.com>
2025-10-08 23:48:29 +0300

    add jobs and candidates page

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ import DashBoard from "./dashboard/page";
 export default function Home() {
   return (
     <main>
-      <h1 className="p-3 bg-amber-600">Hello world</h1>
-      <Link href="/users">
-        <button className="btn btn-primary">Primary</button>
-      </Link>
       <DashBoard />
-
     </main>
   );
 }
